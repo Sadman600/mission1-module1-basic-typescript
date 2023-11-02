bee5ab7dbb9db57e0a3b5967c1bace53244e9bba
Sadman600 <sadmansakib600@gmail.com>
2023-11-02 12:45:49 +0600

    basic data type

---
+++ src/basic-data-type.ts
@@ -0,0 +1,39 @@
+// ** Primitive Data type
+
+// srting
+let fName: string = "Jhon";
+// number
+let salary: number = 25000;
+// boolean
+let isAdult = true;
+// null
+let m: null = null;
+// undefined
+let n: undefined = undefined;
+
+// ** Non Premitive
+
+// Array
+
+// String Array
+let names: string[] = ["a", "b", "c"];
+// Number Array
+let numbers: number[] = [1, 2, 3];
+// Tuple
+let myTuple: [string, number] = ["a", 1];
+// Object Array
+let usersDemo: {}[] = [{ name: "Sakib", age: 29 }];
+let users: { name: string; age: number }[] = [{ name: "Sakib", age: 29 }];
+
+// Object
+const myUser: {
+  name: string;
+  fullName?: string;
+  age: number;
+  isAdult: boolean;
+} = {
+  name: "Jhon",
+  fullName: "Jhon Doe",
+  age: 25,
+  isAdult: true,
+};
