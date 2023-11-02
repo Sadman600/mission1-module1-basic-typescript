d74bfdcb1dd734b02fd663d546d6d9ce6f405590
Sadman600 <sadmansakib600@gmail.com>
2023-11-02 15:57:17 +0600

    add object

---
+++ dist/basic-data-type.js
@@ -0,0 +1,35 @@
+"use strict";
+// ** Primitive Data type
+// srting
+let fName = "Jhon";
+// number
+let salary = 25000;
+// boolean
+let isAdult = true;
+// null
+let m = null;
+// undefined
+let n = undefined;
+// ** Non Premitive
+// Array
+// String Array
+let names = ["a", "b", "c"];
+// Number Array
+let numbers = [1, 2, 3];
+// Tuple
+let myTuple = ["a", 1];
+// Object Array
+let usersDemo = [{ name: "Sakib", age: 29 }];
+let users = [{ name: "Sakib", age: 29 }];
+// // Object
+// const myUser: {
+//   name: string;
+//   fullName?: string;
+//   age: number;
+//   isAdult: boolean;
+// } = {
+//   name: "Jhon",
+//   fullName: "Jhon Doe",
+//   age: 25,
+//   isAdult: true,
+// };
